ec04da890d4ff26204867ad8455e39bf92e4a0e2
Exe <exelcarrizo@gmail.com>
2024-08-30 12:00:52 -0300

    Attributes in Category

--- src/main/java/com/springboot/backend/gestiondeinventarios/models/Category.java
+++ src/main/java/com/springboot/backend/gestiondeinventarios/models/Category.java
@@ -1,6 +1,30 @@
 package com.springboot.backend.gestiondeinventarios.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "notBlank.name.category")
+    private String name;
+
+    @CreationTimestamp
+    private LocalDate create_At;
+
+
 }
